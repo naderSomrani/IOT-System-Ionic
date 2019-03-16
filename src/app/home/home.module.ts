16f0a99ba5e3adb7e0ce142b9c1a359f9644f135
Nader Somrani <nader.somrani1996@gmail.com>
2019-03-16 20:46:33 +0100

    first commit

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,12 +5,21 @@ import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
 
 import { HomePage } from './home.page';
+import { NgxGaugeModule } from 'ngx-gauge';
+import { SocketIoModule, SocketIoConfig } from 'ng-socket-io';
+import { ColorPickerModule } from 'ngx-color-picker';
+
+const config: SocketIoConfig = { url: 'http://localhost:3001', options: {} };
+
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    NgxGaugeModule,
+    SocketIoModule.forRoot(config),
+    ColorPickerModule,
     RouterModule.forChild([
       {
         path: '',
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Socket } from 'ng-socket-io';
+import { Observable } from 'rxjs';
+
+interface messageSocket {
+  gwID: string;
+  nodes: [
+    {nodeID: string,
+      sensors: [{
+        name: string,
+        type: string,
+        value: string}]
+    }
+  ];
+}
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  gaugeType = 'semi';
+  gaugeValue = 28.3;
+  gaugeLabel = 'Temperature';
+  gaugeAppendText = '°C';
 
+  gaugeType2 = 'full';
+  gaugeValue2 = 28.3;
+  gaugeLabel2 = 'Humidity';
+  gaugeAppendText2 = '%';
+
+  gaugeColor = '#E6CF6F';
+
+  ledColor;
+  constructor(private socket: Socket) {
+
+  }
+
+  ngOnInit(): void {
+    this.socket.connect();
+    this.socket.emit('get-sensor-data');
+    this.getMessages().subscribe(message => {
+      console.log(message);
+      this.gaugeValue = Number(message.nodes.filter(node => node.nodeID === '01')[0]
+          .sensors.filter(sens => sens.name === 'temp')[0].value);
+      this.gaugeValue2 = Number(message.nodes.filter(node => node.nodeID === '01')[0]
+          .sensors.filter(sens => sens.name === 'humidity')[0].value);
+      console.log(this.gaugeValue);
+    });
+  }
+
+
+  getMessages() {
+    const observable = new Observable<messageSocket>(observer => {
+      this.socket.on('message', (data) => {
+        observer.next(data);
+      });
+    });
+    return observable;
+  }
+
+  colorChange() {
+    console.log(this.ledColor);
+    this.socket.emit('led-color-change', {color: this.ledColor});
+  }
+
+  ionViewWillLeave() {
+    this.socket.disconnect();
+  }
 }
